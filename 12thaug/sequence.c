db4aa25b8782636ddbe234b9cba988ec150ef2ba
Dhamini Pasam <dhaminipasam3@gmail.com>
2021-08-16 18:49:34 +0530

    commit 2 for getting correct output for all inputs

--- 12thaug/sequence.c
+++ 12thaug/sequence.c
@@ -2,7 +2,10 @@
 #define SIZE 50
 int main()
 {
-	int n,i,j,arr[SIZE],check_bit=0,flag=0,count=0;
+	int n,i,j,num,arr[SIZE];
+	int check_bit,flag,count;
+	
+
 	
 	printf("Enter no.of elements");
 	scanf("%d",&n);
@@ -11,22 +14,34 @@ int main()
 	for(i=0;i<n;i++)
 	scanf("%d",&arr[i]);
 	
+	check_bit=0,flag=0,	count=0;
+	if(n==2 && arr[0]==1 && arr[1]==2)
+	{
+	    flag=1;
+	    goto end;
+	}
+	
 	for(i=1;i<n;i++)				//loop for traversing the array
 	{
-		for(j=0;j<i;j++)            //loop for checking whether sequence is  same as value or not
+	     num=arr[count];
+	    
+		for(j=0;j<num;j++)            //loop for checking whether sequence is  same as value or not
 		{
-		    count++;				
 		    
-		    if(check_bit < n)
+		    count++;
+		    
+		   
+		    if(check_bit < n )
 		    {
 		        
-			    if(arr[check_bit+j]!=i)
+			    if(arr[check_bit+j]!=num)
 			    {
 			      
 			        flag=1;
 			        goto end;			//if false want to go out of two loops so using go to
 			    }
 		    }
+		  
 		}
 		check_bit=count;		//for retaining the index address where  the next element starts in an array
 	}
@@ -36,5 +51,6 @@ end:
 		printf("sequence is invalid\n");
 	else 
 		printf("sequence is valid\n");
+	
 return 0;
 }
